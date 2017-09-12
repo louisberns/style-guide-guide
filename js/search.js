3d34b2a5c07a5033337d4c26feac1dcff94b0e80
louisberns <louisuntitled@gmail.com>
2017-09-12 17:41:27 -0300

    Added logo benner thumbnail

--- js/search.js
+++ js/search.js
@@ -15,7 +15,7 @@
 
       searchResults.innerHTML = appendString;
     } else {
-      searchResults.innerHTML = '<li>No results found</li>';
+      searchResults.innerHTML = '<li>Nenhum resultado encontrado.</li>';
     }
   }
 
